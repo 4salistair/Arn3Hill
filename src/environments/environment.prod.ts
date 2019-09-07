c0b133d66d024724289ef67e0a7d699e494aece5
4salistair <4salistair@gmail.com>
2019-09-07 09:03:15 +0100

    update prod environ file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAc26plafLAKhqK2hj2rituAexam-5HuDY',
+    authDomain: 'three-hill-project.firebaseapp.com',
+    databaseURL: 'https://three-hill-project.firebaseio.com',
+    projectId: 'three-hill-project',
+    storageBucket: 'three-hill-project.appspot.com',
+    messagingSenderId: '756883310155',
+    appId: '1:756883310155:web:fd3d678c767c42a7'
+  },
 };
